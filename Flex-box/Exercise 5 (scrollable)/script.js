a26d6e2814d5eb866aba6a34f2f3837d65bd6451
pczarnecki00 <pczarnecki00@gmail.com>
2021-07-17 14:20:27 +0200

    scrollable

---
+++ Flex-box/Exercise 5 (scrollable)/script.js
@@ -0,0 +1,17 @@
+let data = document.getElementsByClassName('section-item');
+data[data.length -1].style.minWidth = '100%';
+
+for (let item of document.getElementsByClassName('item')) {
+
+    item.addEventListener('click', () => {
+        
+        for (let x of document.getElementsByClassName('item')) {
+            if (x.classList.contains('active')) {
+                x.classList.remove('active')
+            }
+        }
+
+        item.classList.add('active');
+    })
+
+}
