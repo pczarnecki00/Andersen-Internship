ecd7cdfd1bba23d5d6558d365889d2b3c7948083
pczarnecki00 <pczarnecki00@gmail.com>
2021-07-21 14:18:14 +0200

    update todo's

--- Todo-exercise/script.js
+++ Todo-exercise/script.js
@@ -1,46 +1,46 @@
-const toggleFunc = function (a) {
-    let span = a.parentNode.parentNode;
-    span.childNodes[0].classList.toggle('span-complete');
-    span.childNodes[1].childNodes[0].classList.toggle('btn-hovered')
-}
+document.getElementsByTagName('body')[0].addEventListener('click', function(event){
+    //Prevent form submiting
+    event.preventDefault();
 
-const deleteFunc = function (b) {
-    b.parentNode.parentNode.remove();
-}
+    //Access to input value for other variavbles
+    const input = document.querySelector('.input-container__input').value;
 
-const addItem = function () {
-    let li = document.createElement('li');
-    li.classList.add('todo-list__item');
+    //Adds and deletes classes with styles for completed todo's through nodes
+    const toggleFunc = function (a) {
+        a.parentNode.parentNode.childNodes[1].classList.toggle('span-complete');
+        a.parentNode.parentNode.childNodes[3].childNodes[1].classList.toggle('btn-hovered');
+    }
+    
+    //Deletes whole todo's li node 
+    const deleteFunc = function (b) {
+        b.parentNode.parentNode.remove();
+    }
 
-    let liSpan = document.createElement('span');
-    liSpan.classList.add('todo-list__span');
-    liSpan.innerHTML = document.querySelector('.input-container__input').value;
+    //Adds todo's to the list as a li element with it's children and input's value
+    const addItem = function () {
+       
+        const li = document.createElement('li');
+        li.classList.add('todo-list__item');
 
-    let btnPanel = document.createElement('div');
-    btnPanel.classList.add('todo-list__btn-panel');
+        li.innerHTML = ` <span class="todo-list__span">${input}</span>
+        <div class="todo-list__btn-panel">
+            <button class="btn-panel__button btn-complete btn-small">&#10003</button>
+            <button class="btn-panel__button btn-delete btn-small">&#10008</button>
+        </div>`;
 
-    let btnComplete = document.createElement('button');
-    btnComplete.classList.add('btn-panel__button', 'btn-complete', 'btn-small');
-    btnComplete.innerHTML = "&#10003";
-    btnComplete.setAttribute('onClick', "toggleFunc(this)")
+        document.querySelector('.todo-list').append(li);
+    }
 
-    let btnDelete = document.createElement('button');
-    btnDelete.classList.add('btn-panel__button', 'btn-delete', 'btn-small');
-    btnDelete.innerHTML = "&#10008";
-    btnDelete.setAttribute('onClick', 'deleteFunc(this)')
+    const className = event.target.className;
 
-    li.append(liSpan);
-    li.append(btnPanel);
-    btnPanel.append(btnComplete);
-    btnPanel.append(btnDelete);
+    //Ternery  used in order to distinguish which function should be called based on event.target.className properties
+    className.includes('input-container__button')  
+        ? input.trim() && addItem()
+        : className.includes('btn-complete') 
+            ? toggleFunc(event.target) 
+            : className.includes('btn-delete') 
+                ? deleteFunc(event.target) 
+                : '';
 
-    document.querySelector('.todo-list').append(li);
-}
-
-document.querySelector('.input-container__button').addEventListener('click', function () {
-    let input = document.querySelector('.input-container__input').value;
-
-    input.trim() && addItem();
-}
-);
+});
 
