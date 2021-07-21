8c4948d83919d9810ad56869ae2f4e43fd5d2596
pczarnecki00 <pczarnecki00@gmail.com>
2021-07-21 10:12:39 +0200

    todo update

--- Todo-exercise/script.js
+++ Todo-exercise/script.js
@@ -1,48 +1,46 @@
-//Function to update liItem collections and add EventListener to buttons
-function listeners() {
-    liItem = document.getElementsByClassName('todo-list__item');
-    for (let item of liItem) {
-
-        item.childNodes[1].childNodes[0].addEventListener('click', () => {
-            item.childNodes[1].childNodes[0].classList.toggle('btn-hovered')
-            item.childNodes[0].classList.toggle('span-complete');
-        });
-
-        item.childNodes[1].childNodes[1].addEventListener('click', () => {
-            document.querySelector('.todo-list').removeChild(item)
-        })
-    }
+const toggleFunc = function (a) {
+    let span = a.parentNode.parentNode;
+    span.childNodes[0].classList.toggle('span-complete');
+    span.childNodes[1].childNodes[0].classList.toggle('btn-hovered')
 }
 
-// Adds input.value as and li element with children and its attributes
-document.querySelector('.input-container__button').addEventListener('click', () => {
-    if (document.querySelector('.input-container__input').value !== '') {
-        let li = document.createElement('li');
-        li.classList.add('todo-list__item');
+const deleteFunc = function (b) {
+    b.parentNode.parentNode.remove();
+}
 
-        let liSpan = document.createElement('span');
-        liSpan.classList.add('todo-list__span');
-        liSpan.innerHTML = document.querySelector('.input-container__input').value;
+const addItem = function () {
+    let li = document.createElement('li');
+    li.classList.add('todo-list__item');
 
-        let btnPanel = document.createElement('div');
-        btnPanel.classList.add('todo-list__btn-panel');
+    let liSpan = document.createElement('span');
+    liSpan.classList.add('todo-list__span');
+    liSpan.innerHTML = document.querySelector('.input-container__input').value;
 
-        let btnComplete = document.createElement('button');
-        btnComplete.classList.add('btn-panel__button', 'btn-complete', 'btn-small');
-        btnComplete.innerHTML = "&#10003"
+    let btnPanel = document.createElement('div');
+    btnPanel.classList.add('todo-list__btn-panel');
 
-        let btnDelete = document.createElement('button');
-        btnDelete.classList.add('btn-panel__button', 'btn-delete', 'btn-small');
-        btnDelete.innerHTML = "&#10008"
+    let btnComplete = document.createElement('button');
+    btnComplete.classList.add('btn-panel__button', 'btn-complete', 'btn-small');
+    btnComplete.innerHTML = "&#10003";
+    btnComplete.setAttribute('onClick', "toggleFunc(this)")
 
-        li.append(liSpan);
-        li.append(btnPanel);
-        btnPanel.append(btnComplete);
-        btnPanel.append(btnDelete);
+    let btnDelete = document.createElement('button');
+    btnDelete.classList.add('btn-panel__button', 'btn-delete', 'btn-small');
+    btnDelete.innerHTML = "&#10008";
+    btnDelete.setAttribute('onClick', 'deleteFunc(this)')
 
-        document.querySelector('.todo-list').append(li);
-        listeners();
-    }
+    li.append(liSpan);
+    li.append(btnPanel);
+    btnPanel.append(btnComplete);
+    btnPanel.append(btnDelete);
 
-});
+    document.querySelector('.todo-list').append(li);
+}
+
+document.querySelector('.input-container__button').addEventListener('click', function () {
+    let input = document.querySelector('.input-container__input').value;
+
+    input.trim() && addItem();
+}
+);
 
